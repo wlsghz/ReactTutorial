8dce334a89cd20228793883f8ae65c95872cec2a
wlsghz <trollok@naver.com>
2019-03-27 16:19:37 +0900

    Add Get Customer REST API Moduel

--- client/src/App.js
+++ client/src/App.js
@@ -20,35 +20,27 @@ const styles=theme=>({
   }
 })
 
-const customers=[
-{
-  'id': 777,
-  'image':'https://placeimg.com/64/64/1',
-  'name':'유진호',
-  'birthday':'901031',
-  'gender':'man',
-  'job':'사회인'
-},
-{
-  'id': 444,
-  'image':'https://placeimg.com/64/64/2',
-  'name':'유진호',
-  'birthday':'901031',
-  'gender':'man',
-  'job':'사회인'
-},
-{
-  'id': 555,
-  'image':'https://placeimg.com/64/64/3',
-  'name':'유진호',
-  'birthday':'901031',
-  'gender':'man',
-  'job':'사회인'
-}
-]
 
 
 class App extends Component {
+
+  state={
+    customers:""
+  }
+
+  componentDidMount() {
+    this.callApi()
+    .then(res=>this.setState({customers:res}))
+    .catch(err=>console.log(err));
+  }
+
+  callApi = async() => {
+    const response=await fetch('/api/customers');
+    const body=await response.json();
+    return body;
+  }
+
+
   render() {
     const {classes}=this.props;
     return (
@@ -65,7 +57,7 @@ class App extends Component {
           </TableRow>
           </TableHead>
                 <TableBody>
-        {customers.map(c =>{
+        {this.state.customers? this.state.customers.map(c =>{
           return(
                 <Customer
                    key={c.id}
@@ -78,7 +70,7 @@ class App extends Component {
                 />
                  );
              })
-        }
+        :""}
           </TableBody>
           </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port=process.env.PORT||5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello',(req,res)=>{
-    res.send({message:'HelloExpress!'});
+app.get('/api/customers',(req,res)=>{
+   res.send([
+        {
+          'id': 777,
+          'image':'https://placeimg.com/64/64/1',
+          'name':'유진호',
+          'birthday':'901031',
+          'gender':'man',
+          'job':'사회인'
+        },
+        {
+          'id': 444,
+          'image':'https://placeimg.com/64/64/2',
+          'name':'유진호',
+          'birthday':'901031',
+          'gender':'man',
+          'job':'사회인'
+        },
+        {
+          'id': 555,
+          'image':'https://placeimg.com/64/64/3',
+          'name':'유진호',
+          'birthday':'901031',
+          'gender':'man',
+          'job':'사회인'
+        }
+    ])
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
