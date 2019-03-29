8d43750328af976eacf51ae7ebb003543b014ffb
wlsghz <trollok@naver.com>
2019-03-29 11:34:11 +0900

    Add Material UI Modal

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+import DialogContent from '@material-ui/core/DialogContent';
+import Typography from '@material-ui/core/Typography';
 
 class CustomerDelete extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            open:false
+        }
+    }
+
     deleteCustomer(id){
         // 패스배리어블로 사용
         
-        console.log("접근성공");
         const url='/api/customers/'+id;
         fetch(url,{
             method:'DELETE'
@@ -12,9 +25,37 @@ class CustomerDelete extends React.Component{
         this.props.stateRefresh();
     }
 
+    handleClickOpen = () => {
+        this.setState({
+            open:true
+        });
+    }
+
+    handleClose = () =>{
+        this.setState({
+            open:false
+        })
+    }
+
     render(){
         return(
-            <button onClick={(e)=>{this.deleteCustomer(this.props.id)}}>삭제</button>
+            <div>
+            <Button variant="contained" color="secondary" onClick={this.handleClickOpen}>삭제</Button>
+            <Dialog open={this.state.open} onClose={this.handleClose}>
+                <DialogTitle onClose={this.handleClose}>
+                    삭제 경고
+                </DialogTitle>
+                <DialogContent>
+                    <Typography gutterBottom>
+                        선택한 고객 정보가 삭제됩니다.
+                    </Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="contained" color="primary" onClick={(e) => {this.deleteCustomer(this.props.id)}}>삭제</Button>
+                    <Button variant="contained" color="primary" onClick={this.handleClose}>닫기</Button>
+                </DialogActions>
+            </Dialog>
+            </div>
         )
     }
 
